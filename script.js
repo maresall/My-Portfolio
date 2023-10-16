4a939e829af20e31adb3c93bd88428c0fb3054bd
Taner <83558331+maresall@users.noreply.github.com>
2023-10-16 11:52:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const messages = document.getElementById('messages');
+const submit = document.getElementsByClassName('contact1-form')[0];
+
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log(e);
+    console.log(fname);
+
+    Email.send({
+        SecureToken : "f5fff3b8-38cc-41ef-a0e8-868e49a33e3b",
+        To : 'tnr450@gmail.com',
+        From : "tnr450@gmail.com",
+        Subject : "test email",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+});
